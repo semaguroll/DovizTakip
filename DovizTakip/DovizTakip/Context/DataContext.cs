dbd313c988929629e8fd7cd06624c8d998e6a9a1
semaguroll <semaguroll@gmail.com>
2023-03-18 19:51:19 +0300

    Add project files.

---
+++ DovizTakip/DovizTakip/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using DovizTakip.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace DovizTakip.Context
+{
+    public class DataContext : DbContext
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public DataContext(DbContextOptions<DataContext> options, IHttpContextAccessor httpContextAccessor)
+            : base(options)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public DbSet<CurrencyModel> Currencies { get; set; }
+    }
+}
